0cb969a96176d6702ff7f03278a5099961ebfc20
Gandalf Saxe <gandalfsaxe@gmail.com>
2018-05-13 17:36:37 +0200

    cleaned searchclient

--- searchclient_py/searchclient.py
+++ searchclient_py/searchclient.py
@@ -3,7 +3,6 @@
     Date:   2016-02-11
 """
 
-
 import argparse
 import re
 import sys
@@ -21,8 +20,10 @@ from heuristic import AStar, WAStar, Greedy
 
 
 class SearchClient:
-    def __init__(self, server_messages, init_state = None, desired_agent_pos = [None, None]):
+    def __init__(self, server_messages, init_state=None, desired_agent_pos=None):
 
+        if desired_agent_pos is None:
+            desired_agent_pos = [None, None]
         if server_messages is not None:
             self.initial_state = None
 
@@ -48,7 +49,6 @@ class SearchClient:
                         col_dim = len(line)
                     line = server_messages.readline().rstrip()
 
-
                 # Read lines for level.
                 self.info = Info(dims=[row_dim, col_dim])
                 self.initial_state = State(dims=[row_dim, col_dim], info=self.info)
@@ -60,9 +60,10 @@ class SearchClient:
                             self.info.walls[row][col] = True
                         elif char in "0123456789":
                             if self.initial_state.agent_row is not None:
-                                print('Error, encountered a second agent (client only supports one agent).',
-                                      file=sys.stderr,
-                                      flush=True)
+                                print(
+                                    'Error, encountered a second agent (client only supports one agent).',
+                                    file=sys.stderr,
+                                    flush=True)
                                 sys.exit(1)
                             self.initial_state.agent_row = row
                             self.initial_state.agent_col = col
@@ -82,44 +83,41 @@ class SearchClient:
             self.initial_state = init_state
             self.info = Info(dims=[init_state.MAX_ROW, init_state.MAX_COL], agent=desired_agent_pos)
 
-
-
-    def search(self, strategy: 'Strategy') -> '[State, ...]':
+    def search(self, strategy) -> '[State, ...]':
         print('Starting search with strategy {}.'.format(strategy), file=sys.stderr, flush=True)
         strategy.add_to_frontier(self.initial_state)
-        
+
         iterations = 0
         while True:
             if iterations == 1000:
                 print(strategy.search_status(), file=sys.stderr, flush=True)
                 iterations = 0
-            
+
             if memory.get_usage() > memory.max_usage:
                 print('Maximum memory usage exceeded.', file=sys.stderr, flush=True)
                 return None
-            
+
             if strategy.frontier_empty():
                 return None
-            
+
             leaf = strategy.get_and_remove_leaf()
-            
+
             if leaf.is_goal_state():
                 return leaf.extract_plan()
-            
+
             strategy.add_to_explored(leaf)
             for child_state in leaf.get_children():
                 if not strategy.is_explored(child_state) and not strategy.in_frontier(child_state):
                     strategy.add_to_frontier(child_state)
-            
+
             iterations += 1
 
-    def search2(self, strategy: 'Strategy', goalstate) -> '[State, ...]':
+    def search2(self, strategy, goalstate) -> '[State, ...]':
         print('Starting search with strategy {}.'.format(strategy), file=sys.stderr, flush=True)
         strategy.add_to_frontier(self.initial_state)
 
         iterations = 0
         while True:
-
 
             # if iterations >= 1:
             #     print("\033[H\033[J")
@@ -151,7 +149,7 @@ class SearchClient:
 
 
 def main(strat, lvl, log):
-    logName = strftime("%Y-%m-%d-%H-%M", gmtime())
+    log_name = strftime("%Y-%m-%d-%H-%M", gmtime())
     start = time.time()
 
     # Read server messages from stdin.
@@ -160,14 +158,14 @@ def main(strat, lvl, log):
     else:
         server_messages = open("../levels/" + lvl, "r")
 
-
     # Use stderr to print to console through server.
-    print('SearchClient initializing. I am sending this using the error output stream.', file=sys.stderr, flush=True)
-    
+    print('SearchClient initializing. I am sending this using the error output stream.',
+          file=sys.stderr, flush=True)
+
     # Read level and create the initial state of the problem.
     client = SearchClient(server_messages)
 
-    #c2 = SearchClient(server_messages=None, init_state=client.initial_state)
+    # c2 = SearchClient(server_messages=None, init_state=client.initial_state)
 
     if strat == "BFS":
         strategy = StrategyBFS()
@@ -180,16 +178,19 @@ def main(strat, lvl, log):
     elif strat == "astar":
         strategy = StrategyBestFirst(AStar(client.initial_state))
     elif strat == "wstar":
-        strategy = StrategyBestFirst(WAStar(client.initial_state, 5))  # You can test other W values than 5, but use 5 for the report.
+        strategy = StrategyBestFirst(WAStar(client.initial_state,
+                                            5))  # You can test other W values than 5, but use 5 for the report.
     elif strat == "greedy":
         strategy = StrategyBestFirst(Greedy(client.initial_state))
 
     elif strat == "custom":
         strategy = Custom(client.initial_state)
-
+    else:
+        raise Exception("Invalid strategy")
 
     if strat != "custom":
         solution = client.search(strategy)
+
     else:
         solution = strategy.return_solution()
 
@@ -197,41 +198,43 @@ def main(strat, lvl, log):
         print(strategy.search_status(), file=sys.stderr, flush=True)
         print('Unable to solve level.', file=sys.stderr, flush=True)
         sys.exit(0)
-    elif log == False:
+    elif not log:
         print('\nSummary for {}.'.format(strategy), file=sys.stderr, flush=True)
         print('Found solution of length {}.'.format(len(solution)), file=sys.stderr, flush=True)
-        #print('{}.'.format(strategy.search_status()), file=sys.stderr, flush=True)
-        
+        # print('{}.'.format(strategy.search_status()), file=sys.stderr, flush=True)
+
         for state in solution:
             print(state.action, flush=True)
             response = server_messages.readline().rstrip()
             if response == 'false':
-                print('Server responsed with "{}" to the action "{}" applied in:\n{}\n'.format(response, state.action, state), file=sys.stderr, flush=True)
+                print('Server responded with "{}" to the action "{}" applied in:\n{}\n'.format(
+                    response, state.action, state), file=sys.stderr, flush=True)
                 break
     else:
-        #Log info
+        # Log info
         print('Found solution of length {}.'.format(len(solution)), file=sys.stderr, flush=True)
-        with open("logs/" + logName, "a") as myfile:
+        with open("logs/" + log_name, "a") as myfile:
             myfile.write(tabulate([[lvl.ljust(22), format(len(solution)),
-                                          "{0:.2f}".format(time.time() - start)]],
-                                        tablefmt="plain") + "\n")
-        shutil.copy2('strategy.py', "logs/" +  logName + "_strategy")
+                                    "{0:.2f}".format(time.time() - start)]],
+                                  tablefmt="plain") + "\n")
+        shutil.copy2('strategy.py', "logs/" + log_name + "_strategy")
+
 
 if __name__ == '__main__':
     # Program arguments.
     parser = argparse.ArgumentParser(description='Simple client based on state-space graph search.')
-    parser.add_argument('--max_memory', metavar='<MB>', type=float, default=512.0, help='The maximum memory usage allowed in MB (soft limit, default 512).')
-    parser.add_argument('--strategy', default="BFS", help='The chosen strategy BFS | DFS | ASTAR | WASTAR | GREEDY')
-    parser.add_argument('--lvl', default="", help="Choose to input lvl, mode made when running without server")
-    parser.add_argument('--log', default = False, help="Log to file")
+    parser.add_argument('--max_memory', metavar='<MB>', type=float, default=512.0,
+                        help='The maximum memory usage allowed in MB (soft limit, default 512).')
+    parser.add_argument('--strategy', default="BFS",
+                        help='The chosen strategy BFS | DFS | ASTAR | WASTAR | GREEDY')
+    parser.add_argument('--lvl', default="",
+                        help="Choose to input lvl, mode made when running without server")
+    parser.add_argument('--log', default=False, help="Log to file")
     args = parser.parse_args()
-    
+
     # Set max memory usage allowed (soft limit).
     memory.max_usage = args.max_memory
-    strat = args.strategy
+    my_strat = args.strategy
 
     # Run client.
-    main(strat, args.lvl, args.log)
-
-
-
+    main(my_strat, args.lvl, args.log)
