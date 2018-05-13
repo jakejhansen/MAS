32328e95228c7c3641d7815e6d18e6d058603052
Gandalf Saxe <gandalfsaxe@gmail.com>
2018-05-13 15:42:32 +0200

    cornerfinder now has `is_corner()` function

--- searchclient_py/corner_finder.py
+++ searchclient_py/corner_finder.py
@@ -1,22 +1,22 @@
-import numpy as np
-from constants import *
 from import_level import *
 from pathfinder import pathfinder
+import copy
+
 
 def corner_finder(walls, goals, print_corners=False):
-    '''Find all corners of a level.
+    """Find all corners of a level.
     A corner is defined: cell c with the property that if there is a path from
     A to B (with A ≠ c and B ≠ c) in the level, then there is a path that
-    doesn’t go through c.
+    doesn't go through c.
     Or as implemented here:
-    1. Corner-candidates: have two touching walls as neighbors
+    1. Corner-candidates: have two diagonally touching walls as neighbors
     2. Corners: corner-candidates where there is a path between two non-wall
-    neighbors that doens't go through c.
+    neighbors that doesn't go through c.
         INPUT:
             - filename: filename of level [string]
             - print_corners: if true, then prints level for X for corners
         OUTPUT: tuple of list of coordinates: (corners,corner_goals)
-    '''
+    """
 
     raw = np.zeros_like(walls, dtype='object')
 
@@ -25,73 +25,14 @@ def corner_finder(walls, goals, print_corners=False):
     corners = []
     corner_goals = []
     # we skip first/last row/col (these are either walls or not part of level)
-    for i in range(1, nrows-1):
-        for j in range(1, ncols-1):
+    for i in range(1, nrows - 1):
+        for j in range(1, ncols - 1):
 
-            # Pass if current cell is a wall
-            if walls[i,j] == 1:
-                continue
-            else:
-                neighbor_N = neighbor_S = neighbor_E = neighbor_W = 0
-                # Neighbor coordinates
-                N = (i-1, j)
-                S = (i+1, j)
-                E = (i, j+1)
-                W = (i, j-1)
-                # Neighbors are either 0 (free) or 1 (walls)
-                neighbor_N = walls[N]
-                neighbor_S = walls[S]
-                neighbor_W = walls[W]
-                neighbor_E = walls[E]
-
-                # if three neighbors are walls, then (i,j) is corner point (and a
-                # "blind-alley point")
-                if neighbor_N + neighbor_S + neighbor_E + neighbor_W == 3:
-                    if goals[i,j] == 1:
-                        corner_goals.append((i,j))
-                    else:
-                        corners.append((i,j))
-
-                # if two neighbors are touching, see if it's a corner
-                elif  (neighbor_W + neighbor_N) == 2:
-                        # try to find path between S and E neighbors
-                        walls[i,j] = 1  # add temp wall at corner candidate
-                        if pathfinder(walls, S, E):
-                            if goals[i,j] == 1:
-                                corner_goals.append((i,j))
-                            else:
-                                corners.append((i,j))
-                        walls[i,j] = 0  # remove temp wall at corner candidate
-
-                elif  (neighbor_N + neighbor_E) == 2:
-                        # try to find path between S and W neighbors
-                        walls[i,j] = 1  # add temp wall at corner candidate
-                        if pathfinder(walls, S, W):
-                            if goals[i,j] == 1:
-                                corner_goals.append((i,j))
-                            else:
-                                corners.append((i,j))
-                        walls[i,j] = 0  # remove temp wall at corner candidate
-
-                elif  (neighbor_S + neighbor_E) == 2:
-                        # try to find path between N and W neighbors
-                        walls[i,j] = 1  # add temp wall at corner candidate
-                        if pathfinder(walls, N, W):
-                            if goals[i,j] == 1:
-                                corner_goals.append((i,j))
-                            else:
-                                corners.append((i,j))
-                        walls[i,j] = 0  # remove temp wall at corner candidate
-
-                elif  (neighbor_S + neighbor_W) == 2:
-                    # try to find path between N and E neighbors
-                        walls[i,j] = 1  # add temp wall at corner candidate
-                        if pathfinder(walls, N, E):
-                            if goals[i,j] == 1:
-                                corner_goals.append((i,j))
-                            else:
-                                corners.append((i,j))
-                        walls[i,j] = 0  # remove temp wall at corner candidate
+            if is_corner(walls, [i, j]):
+                if goals[i, j] == 1:
+                    corner_goals.append((i, j))
+                else:
+                    corners.append((i, j))
 
     if print_corners:
         print('ROWS: {}'.format(nrows))
@@ -113,26 +54,62 @@ def corner_finder(walls, goals, print_corners=False):
         print("Corner-goals:")
         print(corner_goals)
         print()
-    return (corners, corner_goals)
+    return corners, corner_goals
 
+
+def is_corner(walls, position) -> bool:
+    """ Return True if position is a corner, False otherwise."""
+    i, j = position
+    is_corner = False
+
+    # Special case #1: Always false if cell is a wall
+    if walls[i, j] == 1:
+        return False
+
+    # Neighbor relative coordinates
+    n = (i - 1, j)
+    s = (i + 1, j)
+    e = (i, j + 1)
+    w = (i, j - 1)
+    # Neighbors are either 0 (non-wall) or 1 (walls)
+    neighbor_n = walls[n]
+    neighbor_s = walls[s]
+    neighbor_w = walls[w]
+    neighbor_e = walls[e]
+
+    # Special case #2: Always true if three neighbors are walls
+    if neighbor_n + neighbor_s + neighbor_e + neighbor_w == 3:
+        return True
+
+    # add temp wall at corner candidate, try to find path between neighbors pairwise
+    walls[i,j] = 1
+
+    if (neighbor_w + neighbor_n) == 2:
+        if pathfinder(walls, s, e):
+            is_corner = True
+
+    elif (neighbor_n + neighbor_e) == 2:
+        if pathfinder(walls, s, w):
+            is_corner = True
+
+    elif (neighbor_s + neighbor_e) == 2:
+        if pathfinder(walls, n, w):
+            is_corner = True
+
+    elif (neighbor_s + neighbor_w) == 2:
+        if pathfinder(walls, n, e):
+            is_corner = True
+
+    walls[i,j] = 0
+
+    return is_corner
 
 
 if __name__ == "__main__":
-    print("HEJ0")
-    # corner_finder('pathfinderTest.lvl', print_corners=True)
-    # corner_finder('SAlabyrinth.lvl', print_corners=True)
-    # corner_finder('SAsimple3.lvl', print_corners=True)
-    # corner_finder('SAsokobanLevel96.lvl', print_corners=True)
-    test = corner_finder('SAsokobanLevel96.lvl', print_corners=True)
+    # filename = 'SAlabyrinth.lvl'
+    filename = 'SAsimple3.lvl'
+    # filename = 'SAsokobanLevel96.lvl'# import_level()
 
+    _, test_walls, test_goals, _, _, test_raw = import_level(filename, printout=[1, 0, 0, 0, 0])
 
-
-
-
-
-
-
-
-
-
-
+    test = corner_finder(test_walls, test_goals, print_corners=True)
