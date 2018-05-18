516eba46c0f735602acc884f2424ea323b078290
Gandalf Saxe <gandalfsaxe@gmail.com>
2018-05-18 16:04:40 +0200

    fixed logfix

--- searchclient_py/logfix.py
+++ searchclient_py/logfix.py
@@ -1,15 +1,19 @@
 import os
 from tabulate import tabulate
-logName = os.listdir("./logs")
-logName = sorted(logName)[-2]
-res = []
-for line in open("logs/" + logName):
-    res.append(line.split())
+logNames = os.listdir("./logs")
 
 
-    
-with open("logs/" + logName, "w") as myfile:
-    myfile.write(tabulate(res, ["lvl", "len", "time"], tablefmt="plain"))
+for logName in sorted(logNames):
+    if "_strategy" not in logName and ".DS_Store" not in logName:
 
-    
-print(tabulate(res, ["lvl", "len", "time"], tablefmt="plain"))
+        res = []
+        for line in open("logs/" + logName):
+            res.append(line.split())
+
+
+
+        with open("logs/" + logName, "w") as myfile:
+            myfile.write(tabulate(res, ["lvl", "len", "time"], tablefmt="plain"))
+
+
+        print(tabulate(res, ["lvl", "len", "time"], tablefmt="plain"))
